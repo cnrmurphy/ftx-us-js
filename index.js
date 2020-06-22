256ae09ea705aac26f0a9083445f03ca829ca33c
cjm322 <conor.murphy@gmail.com>
2020-06-22 00:08:28 -0400

    added endpoint examples for getWallet and getMarkets:

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const FTXUS = require('./FTXUS');
 const endpoints = require('./endpoints');
 const apiKey = process.env.FTX_API_KEY;
 const apiSecret = process.env.FTX_API_SECRET;
+const ftxUs = new FTXUS({ key: apiKey, secret: apiSecret });
 
 (async () => {
   try {
-    let ftxUs = new FTXUS({ key: apiKey, secret: apiSecret });
-    
-    ftxUs.createRequest('get', endpoints.WALLET.ALL_BALANCES);
-    console.log(await ftxUs._client.get(endpoints.WALLET.ALL_BALANCES));
+    const wallet = await getWallet();
+    const markets = await getMarkets();
+    wallet.main.forEach(asset => {
+      console.log(asset);
+    });
+
+    markets.forEach(market => {
+      console.log(market);
+    });
 
   } catch(e) {
     console.log(e);
   }
 })();
+
+async function getWallet() {
+   ftxUs.createRequest('GET', endpoints.WALLET.ALL_BALANCES);
+   const walletResp = await ftxUs._client.get(endpoints.WALLET.ALL_BALANCES);
+   const wallet = walletResp.data.result;
+   return wallet;
+}
+
+async function getMarkets() {
+    ftxUs.createRequest('GET', '/markets');
+    const resp = await ftxUs._client.get('/markets');
+    const markets = resp.data.result;
+    return markets;
+}
