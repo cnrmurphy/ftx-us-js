1b3bd7604b6a3cab12ec81282a4dffd6d15e8154
cjm322 <conor.murphy@gmail.com>
2020-06-21 22:40:48 -0400

    completed signRequest

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const crypto = require('crypto');
+const URL = require('url');
 
 const apiKey = process.env.FTX_API_KEY;
 const apiSecret = process.env.FTX_API_SECRET;
@@ -9,23 +11,36 @@ const FTXUS_SIGN = 'FTXUS-SIGN';
 const FTXUS_SUBACCOUNT = 'FTXUS-SUBACCOUNT';
 
 const defaultURL = 'ftx.us/api';
+const defaultTimeout = 10;
 
 class FTXUS {
   constructor({
     key,
-    secret
+    secret,
+    client=null
   }) {
     this._key = key;
     this._secret = secret;
-    this._client = axios;
+    this._client = client || axios.create({
+      baseURL: defaultURL,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      timeout: defaultTimeout
+    });
     this._subaccount = null;
   };
 
   signRequest() {
     const headers = this._client.default.headers.common;
     const timestamp = headers[FTXUS_TS];
-
-
+    const url = new URL(this._client.url);
+    const signaturePayload = `${timestamp}${this._client.method}${url.pathname}`;
+    const signature = crypto.createHmac('sha256', this._secret)
+      .update(signaturePayload)
+      .digest('hex');
+    
+    headers[FTXUS_SIGN] = signature;
   }
 
   createRequest(method, endpoint) {
@@ -34,6 +49,8 @@ class FTXUS {
     }
     this._client.default.headers.common[FTXUS_KEY] = this._key;
     this._client.default.headers.common[FTXUS_TS] = Date.now();
-
+    this.signRequest();
   }
 }
+
+
