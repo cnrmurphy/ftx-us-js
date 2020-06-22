1cb9c75fc67936a599641a9c8963f685ded6840f
cjm322 <conor.murphy@gmail.com>
2020-06-21 23:04:03 -0400

    moved client code to its own file and added initilization example to index.js

--- index.js
+++ index.js
@@ -1,56 +1,16 @@
-const axios = require('axios');
-const crypto = require('crypto');
-const URL = require('url');
-
+const FTXUS = require('./FTXUS');
+const endpoints = require('./endpoints');
 const apiKey = process.env.FTX_API_KEY;
 const apiSecret = process.env.FTX_API_SECRET;
 
-const FTXUS_KEY = 'FTXUS-KEY';
-const FTXUS_TS = 'FTXUS-TS';
-const FTXUS_SIGN = 'FTXUS-SIGN';
-const FTXUS_SUBACCOUNT = 'FTXUS-SUBACCOUNT';
-
-const defaultURL = 'ftx.us/api';
-const defaultTimeout = 10;
-
-class FTXUS {
-  constructor({
-    key,
-    secret,
-    client=null
-  }) {
-    this._key = key;
-    this._secret = secret;
-    this._client = client || axios.create({
-      baseURL: defaultURL,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      timeout: defaultTimeout
-    });
-    this._subaccount = null;
-  };
-
-  signRequest() {
-    const headers = this._client.default.headers.common;
-    const timestamp = headers[FTXUS_TS];
-    const url = new URL(this._client.url);
-    const signaturePayload = `${timestamp}${this._client.method}${url.pathname}`;
-    const signature = crypto.createHmac('sha256', this._secret)
-      .update(signaturePayload)
-      .digest('hex');
+(async () => {
+  try {
+    let ftxUs = new FTXUS({ key: apiKey, secret: apiSecret });
     
-    headers[FTXUS_SIGN] = signature;
+    ftxUs.createRequest('get', endpoints.WALLET.ALL_BALANCES);
+    console.log(await ftxUs._client.get(endpoints.WALLET.ALL_BALANCES));
+
+  } catch(e) {
+    console.log(e);
   }
-
-  createRequest(method, endpoint) {
-    if (this._subaccount) {
-      this.client.default.headers.common[FTXUS_SUBACCOUNT] = this._subaccount;
-    }
-    this._client.default.headers.common[FTXUS_KEY] = this._key;
-    this._client.default.headers.common[FTXUS_TS] = Date.now();
-    this.signRequest();
-  }
-}
-
-
+})();
