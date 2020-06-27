587fa4fb7979b7c0cbdfb802a0fa5c9c845bd60c
cjm322 <conor.murphy@gmail.com>
2020-06-27 19:01:40 -0400

    moved service methods to their own class. Changed requestSignature to return a signature rather than mutating a given header. Added get and post method. Added url library

--- FTXUS.js
+++ FTXUS.js
@@ -2,6 +2,10 @@ const axios = require('axios');
 const crypto = require('crypto');
 const { URL } = require('url');
 
+const WalletService = require('./Wallet');
+const MarketsService = require('./Markets');
+const OrdersService = require('./Orders');
+
 const FTXUS_KEY = 'FTXUS-KEY';
 const FTXUS_TS = 'FTXUS-TS';
 const FTXUS_SIGN = 'FTXUS-SIGN';
@@ -12,12 +16,13 @@ const defaultTimeout = 1000;
 
 class FTXUS {
   constructor({
-    key,
-    secret,
-    client=null
+    key='',
+    secret='',
+    client=null,
   }) {
     this._key = key;
     this._secret = secret;
+    this._baseURL = new URL(defaultURL);
     this._client = client || axios.create({
       baseURL: defaultURL,
       headers: {
@@ -26,27 +31,67 @@ class FTXUS {
       timeout: defaultTimeout
     });
     this._subaccount = null;
+    this._wallet = new WalletService(this);
+    this._markets = new MarketsService(this);
+    this._orders = new OrdersService(this);
   };
 
-  signRequest(method, endpoint) {
-    const headers = this._client.defaults.headers.common;
-    const timestamp = headers[FTXUS_TS];
-    const url = new URL(this._client.defaults.baseURL + endpoint);
-    const signaturePayload = `${timestamp}${method}${url.pathname}`;
+  get Wallet(){
+    return this._wallet;
+  }
+
+  get Markets(){
+    return this._markets;
+  }
+
+  get Orders() {
+    return this._orders;
+  }
+
+  get baseURL() {
+    return this._baseURL;
+  }
+
+  set client(client){
+    this._client = client;
+  }
+
+  requestSignature(ts, method, url) {
+    const signaturePayload = `${ts}${method}/api${url.pathname}${url.search}`;
     const signature = crypto.createHmac('sha256', this._secret)
       .update(signaturePayload)
       .digest('hex');
-    
-    headers[FTXUS_SIGN] = signature;
+    return signature;
   }
 
-  createRequest(method, endpoint) {
+  createRequest(method, url) {
     if (this._subaccount) {
       this.client.defaults.headers.common[FTXUS_SUBACCOUNT] = this._subaccount;
     }
-    this._client.defaults.headers.common[FTXUS_KEY] = this._key;
-    this._client.defaults.headers.common[FTXUS_TS] = +new Date().toString();
-    this.signRequest(method, endpoint);
+    if (this.clientCanAuthorize()) {
+      const ts = +Date.toString();
+      this._client.defaults.headers.common[FTXUS_KEY] = this._key;
+      this._client.defaults.headers.common[FTXUS_TS] = ts;
+      this._client.defaults.headers.common[FTXUS_SIGN] = this.requestSignature(ts, method, url);
+    }
+  }
+
+  clientCanAuthorize() {
+    if (this._key.length > 0 && this._secret.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get(url) {
+    this.createRequest('GET', url);
+    return this._client.get(url.pathname+url.search);
+  }
+
+  post(url, payload) {
+    this.createRequest('POST', url);
+    return this._client.post(url.pathname, payload);
   }
 }
 
