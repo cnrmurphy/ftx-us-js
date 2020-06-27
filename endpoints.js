11a4e836a13a6bd544647daf20af804aba7a0d1b
cjm322 <conor.murphy@gmail.com>
2020-06-27 19:05:52 -0400

    added new endpoints

--- endpoints.js
+++ endpoints.js
@@ -1,5 +1,20 @@
 module.exports = {
+  SUBACCOUNTS: {
+    DEFAULT: '/subaccounts',
+    UPDATE_NAME: '/subaccounts/update_name',
+    TRANSFER: '/subaccounts/transfer'
+  },
+  MARKETS: {
+    DEFAULT: '/markets',
+    ORDERBOOK: '/orderbook?depth='            // /markets/{market_name}/orderbook?depth={depth}
+  },
   WALLET: {
-    ALL_BALANCES: '/wallet/all_balances'
-  }
+    DEFAULT: '/wallet',
+    COINS: '/wallet/coins',
+    BALANCES: '/wallet/balances',
+    DEPOSIT_ADDRESS: '/wallet/deposit_address',
+    ALL_BALANCES: '/wallet/all_balances',
+    DEPOSITS: '/wallet/deposits',
+    WITHDRAWALS: '/wallet/withdrawals'
+  },
 }
