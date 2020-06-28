b1bad84f2567f799d4e57b0d05736914291fc98f
8bitstout <conor.murphy4304@gmail.com>
2020-06-27 21:27:31 -0400

    changed index to export client and useful string constants

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const FTXUS = require('./FTXUS');
+const endpoints = require('./endpoints');
+const currencyPairs = require('./currencyPairs');
 
-const apiKey = process.env.FTX_API_KEY;
-const apiSecret = process.env.FTX_API_SECRET;
-
-const ftxUs = new FTXUS({ key: apiKey, secret: apiSecret });
-
-(async () => {
-  try { 
-    const m = ftxUs.Markets;
-    console.log(await m.list());
-  } catch(e) {
-    console.log(e);
-  }
-})();
+module.exports = {
+  FTXUS,
+  endpoints,
+  currencyPairs
+}
