2cdc45caddbd05095cf211b34b5c68542ad50479
8bitstout <conor.murphy4304@gmail.com>
2020-06-28 23:01:06 -0400

    fixed incorrect import

--- example.js
+++ example.js
@@ -1,13 +1,13 @@
-const FTXUS = require('./FTXUS');
+const { FTXUS } = require('./FTXUS');
 
 const apiKey = process.env.FTX_API_KEY;
 const apiSecret = process.env.FTX_API_SECRET;
 
-const ftxUs = new FTXUS({ key: apiKey, secret: apiSecret });
+const ftxUS = new FTXUS({ key: apiKey, secret: apiSecret });
 
 (async () => {
   try { 
-    const m = ftxUs.Markets;
+    const m = ftxUS.Markets;
     console.log(await m.list());
   } catch(e) {
     console.log(e);
