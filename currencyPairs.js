9ae9f70020a2e179a7b26b1ffea8df885fdf13de
8bitstout <conor.murphy4304@gmail.com>
2020-06-28 23:46:06 -0400

    added string constant values for currency pairs

---
+++ currencyPairs.js
@@ -0,0 +1,19 @@
+module.exports = {
+  BTC: {
+    USD: 'BTC/USD',
+    USDT: 'BTC/USDT'
+  },
+  ETH: {
+    USD: 'ETH/USD',
+    USDT: 'ETH/USDT',
+    BTC: 'ETH/BTC'
+  },
+  LTC: {
+    USD: 'LTC/USD',
+    USDT: 'LTC/USDT'
+  },
+  BCH: {
+    USD: 'BCH/USD',
+    USDT: 'BCH/USDT'
+  }
+}
