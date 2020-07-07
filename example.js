4d4db2d05447eac89c98bd5258ef79109539ae59
8bitstout <conor.murphy4304@gmail.com>
2020-07-07 09:41:30 -0400

    updated example

--- example.js
+++ example.js
@@ -1,14 +1,13 @@
-const { FTXUS } = require('./FTXUS');
+const { FTXUS, currencyPairs } = require('./index');
 
 const apiKey = process.env.FTX_API_KEY;
 const apiSecret = process.env.FTX_API_SECRET;
-
 const ftxUS = new FTXUS({ key: apiKey, secret: apiSecret });
 
 (async () => {
   try { 
-    const m = ftxUS.Markets;
-    console.log(await m.list());
+    const fills = ftxUS.Fills;
+    console.log(await fills.list({ limit: 10, market: currencyPairs.BTC.USD }));
   } catch(e) {
     console.log(e);
   }
